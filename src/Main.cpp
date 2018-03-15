9d2d0151c64162622249911e6bd4ee61c5f5c8d8
Richard <richard.grieves@gmail.com>
2018-03-15 19:10:16 +0200

    First commit to add a pixel

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,9 +7,21 @@
 
 
 #include <iostream>
+#include <string>
 #include <SDL.h>
+#include <vector>
+
+const int WIDTH = 640;
+const int HEIGHT = 480;
 
 using namespace std;
+
+int rows(int r){
+	return WIDTH*4*(r-1) ;
+}
+int columns(int c){
+	return (4*(c-1)) ;
+}
 
 int main(){
 
@@ -18,50 +30,50 @@ int main(){
 		return 1;
 	}
 
-	SDL_Window *win = SDL_CreateWindow("Hello World!",100,100,640,480,SDL_WINDOW_SHOWN);
 
-	if (win == NULL){
-		cout << "SDL Error: " << SDL_GetError() << endl;
-		SDL_Quit();
-		return 1;
-	}
+	SDL_Window *win = SDL_CreateWindow("Hello World!",100,100,WIDTH,HEIGHT,0);
+
 
 	SDL_Renderer *ren = SDL_CreateRenderer(win,-1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	if (ren==NULL){
-		SDL_DestroyWindow(win);
-		cout << "SDL_CreateRenderer Error: " << SDL_GetError() << endl;
-		SDL_Quit();
-		return 1;
-	}
+	SDL_Texture *tex = SDL_CreateTexture(ren,SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, WIDTH,HEIGHT);
+	bool quit = false;
+	SDL_Event event;
 
-	SDL_Surface *bmp = SDL_LoadBMP("/home/richard/hello.bmp");
+	vector< unsigned char > pixels(WIDTH*HEIGHT*4,0);
+	unsigned char setPixel[4]={255,255,255,255};
 
-	if (bmp==NULL){
-			SDL_DestroyRenderer(ren);
-			SDL_DestroyWindow(win);
-			cout << "SDL_LoadBMP Error: " << SDL_GetError() << endl;
-			SDL_Quit();
-			return 1;
-		}
+	std::copy(setPixel,setPixel+4,&pixels[rows(240)+columns(320)]); //one way to set a group
 
-	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren,bmp);
-	SDL_FreeSurface(bmp);
-	if (tex==NULL){
-				SDL_DestroyRenderer(ren);
-				SDL_DestroyWindow(win);
-				cout << "SDL_CTFS Error: " << SDL_GetError() << endl;
-				SDL_Quit();
-				return 1;
-			}
+	/*const unsigned int x = rand() % texWidth;
+            const unsigned int y = rand() % texHeight;
 
-	SDL_RenderClear(ren);
-	SDL_RenderCopy(ren,tex,NULL,NULL);
+            const unsigned int offset = ( texWidth * 4 * y ) + x * 4;
+            pixels[ offset + 0 ] = rand() % 256;        // b
+            pixels[ offset + 1 ] = rand() % 256;        // g
+            pixels[ offset + 2 ] = rand() % 256;        // r
+            pixels[ offset + 3 ] = SDL_ALPHA_OPAQUE;    // a                 another way to set a group*/
 
-	SDL_RenderPresent(ren);
+	while (!quit)
+	    {
+		SDL_UpdateTexture(tex,NULL,&pixels[0],640*sizeof(Uint32));
 
-	SDL_Delay(3000);
-	//SDL_DestroyTexture(tex);
+	        SDL_WaitEvent(&event);
+	        switch (event.type)
+	        {
+	            case SDL_QUIT:
+	                quit = true;
+	                break;
+	        }
+
+	        SDL_RenderClear(ren);
+	        SDL_RenderCopy(ren,tex,NULL,NULL);
+	        SDL_RenderPresent(ren);
+	    }
+
+
+
+	SDL_DestroyTexture(tex);
 	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
